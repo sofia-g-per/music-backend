3a5e5ee4d5349644608931f3d2edef75e3455ecb
Sofia <sofia.g.per@gmail.com>
2022-04-10 17:56:59 +0300

    Добавлена конфигурация для автоматической перекомпиляции при разработке

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import * as session from 'express-session';
 import * as passport from 'passport';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(
@@ -21,5 +23,11 @@ async function bootstrap() {
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
+
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
